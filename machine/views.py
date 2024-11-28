a429496f8bfb3e2229367c758bee96a0fef50c3a
LYUPX <104468586+Felipelima2001@users.noreply.github.com>
2024-11-27 21:22:01 -0300

    Descrição da avaliaçao de modelo

--- machine/views.py
+++ machine/views.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 from django.core.files.storage import FileSystemStorage
+from sklearn.metrics import r2_score
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor, RandomForestClassifier
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
@@ -14,38 +15,28 @@ import plotly.express as px
 from io import BytesIO
 import base64
 from django.http import JsonResponse
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 from django.conf import settings
 
 
-def generate_scatter_plot(x, y):
-    fig, ax = plt.subplots()
-    ax.scatter(x, y, color='blue')
-    ax.set_xlabel('Feature')
-    ax.set_ylabel('Target')
-    ax.set_title('Gráfico Scatter de predição')
+def generate_text_response(y_test, y_pred):
+    # Exemplo de cálculo de erro para retornar um texto
+    mse = mean_squared_error(y_test, y_pred)
+    if mse < 10:
+        return f"Previsões precisas com um MSE de {mse:.2f}."
+    elif mse < 50:
+        return f"Previsões razoáveis com um MSE de {mse:.2f}."
+    else:
+        return f"As previsões têm um alto erro com um MSE de {mse:.2f}."
 
-    # Cria o diretório "plots" dentro da pasta "media", caso não exista
-    plot_dir = os.path.join(settings.BASE_DIR, 'media')
-    os.makedirs(plot_dir, exist_ok=True)
-
-    # Define o caminho do arquivo onde a imagem será salva
-    plot_path = os.path.join(plot_dir, 'scatter_plot.png')
-
-    # Salva o gráfico no arquivo
-    fig.savefig(plot_path)
-    plt.close(fig)
-
-    # Retorna o caminho da imagem salva
-    return plot_path
 
 def machine(request):
     if request.method == 'POST':
 
         try:
             # Recebe os dados do frontend
-            X = ""
-            y = ["", "", ""]
+            y = request.POST.get('target')
+            x = request.POST.getlist('features')
             tarefa = request.POST.get('tarefa')
             modelo = request.POST.get('modelo')
             max_depth = int(request.POST.get('max_depth'))
@@ -88,24 +79,31 @@ def machine(request):
             # Faz previsões
             y_pred = model.predict(X_test)
 
-            # Calcular o erro médio quadrático (MSE) para avaliação
+# Calcular métricas de avaliação
             mse = mean_squared_error(y_test, y_pred)
+            mae = mean_absolute_error(y_test, y_pred)
+            r2 = r2_score(y_test, y_pred)
+            
+            texto_explicativo = f"""
+            O modelo selecionado foi o {modelo} para a tarefa de {tarefa}. 
+            O target escolhido foi {y} e as features selecionadas foram {', '.join(x)}.
+            O erro médio quadrático (MSE) foi de {mse:.2f}, o que indica quão bem o modelo se ajusta aos dados. 
+            Quanto menor o MSE, melhor o modelo. 
+            O coeficiente de determinação R² foi de {r2:.2f} o que significa que {r2*100:.2f}% da variabilidade dos dados pode ser explicada pelo modelo.
+            """
 
-            # Gerar gráfico de dispersão das previsões vs valores reais
-            img_str = generate_scatter_plot(y_test, y_pred)
 
-            # Retorna a resposta com o MSE e a imagem do gráfico
-            return redirect('http://127.0.0.1:8000/machine/')
-        
+
+            return render(request, 'machine/Machine.html', {
+                "mse": mse,
+                "mae": mae,
+                "r2": r2,
+                "texto_explicativo": texto_explicativo
+            })
+
+
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
     else:
-        fs = FileSystemStorage(location='media')
-
-        graph_filename_three = 'scatter_plot.png'
-        graph_path_three = os.path.join(fs.location, graph_filename_three)
-        scatter_plot = fs.url(graph_filename_three)
-
-        return render(request, 'machine/Machine.html', { "scatter_plot_url": scatter_plot })
-    
+        return render(request, 'machine/Machine.html')
  
